c144836282b8c534d403d9d472a66280141659de
Andrew Ray <me@andrewray.me>
2023-08-28 20:24:26 -0700

    Orbit camera and light helpers and rotating point lights

--- src/editor-engine-plugins/playcanvas/usePlayCanvas.tsx
+++ src/editor-engine-plugins/playcanvas/usePlayCanvas.tsx
@@ -1,5 +1,11 @@
 import { useCallback, useEffect, useRef, useState, useContext } from 'react';
 import * as pc from 'playcanvas';
+import {
+  OrbitCamera,
+  OrbitCameraInputMouse,
+  OrbitCameraInputTouch,
+  OrbitCameraInputKeyboard,
+} from './OrbitCamera';
 import { useHoisty } from '../../editor/hoistedRefContext';
 import { physicalDefaultProperties } from '@core/plugins/playcanvas/playengine';
 
@@ -12,6 +18,10 @@ type SceneData = {
 };
 type ScenePersistence = {
   sceneData: SceneData;
+  orbitCamera: OrbitCamera;
+  orbitCameraInputMouse: OrbitCameraInputMouse;
+  orbitCameraInputTouch: OrbitCameraInputTouch;
+  orbitCameraInputKeyboard: OrbitCameraInputKeyboard;
   canvas: HTMLCanvasElement;
   pcDom: HTMLDivElement;
   app: pc.Application;
@@ -24,12 +34,24 @@ type Callback = (time: number) => void;
 export const usePlayCanvas = (callback: Callback) => {
   const { getRefData } = useHoisty();
 
-  const { canvas, loadingMaterial, app, camera, sceneData } = getRefData<
-    Omit<ScenePersistence, 'pcDom'>
-  >('playcanvas', () => {
+  const {
+    orbitCamera,
+    orbitCameraInputMouse,
+    orbitCameraInputTouch,
+    orbitCameraInputKeyboard,
+    canvas,
+    loadingMaterial,
+    app,
+    camera,
+    sceneData,
+  } = getRefData<Omit<ScenePersistence, 'pcDom'>>('playcanvas', () => {
     const canvas = document.createElement('canvas');
 
-    const app = new pc.Application(canvas);
+    const app = new pc.Application(canvas, {
+      mouse: new pc.Mouse(canvas),
+      touch: new pc.TouchDevice(canvas),
+      keyboard: new pc.Keyboard(window),
+    });
     // fill the available space at full resolution
     app.setCanvasFillMode(pc.FILLMODE_NONE);
     app.setCanvasResolution(pc.RESOLUTION_AUTO);
@@ -38,10 +60,44 @@ export const usePlayCanvas = (callback: Callback) => {
 
     const camera = new pc.Entity('camera');
     camera.addComponent('camera', {
-      clearColor: new pc.Color(0.1, 0.1, 0.1),
+      fov: 75,
+      frustumCulling: true,
+      clearColor: new pc.Color(0, 0, 0, 0),
     });
+    camera.setPosition(0, 0, 6);
+
+    // All of the orbitcamera stuff is hacked up from
+    // https://github.com/playcanvas/model-viewer/blob/61e8f51207dd8e7a676e1d55cb70e894fa20e337/src/viewer.ts#L166
+    const origMouseHandler = app.mouse._moveHandler;
+    app.mouse.detach();
+    app.mouse._moveHandler = (event: MouseEvent) => {
+      if (event.target === canvas) {
+        origMouseHandler(event);
+      }
+    };
+    app.mouse.attach(canvas);
+
+    const origTouchHandler = app.touch._moveHandler;
+    app.touch.detach();
+    app.touch._moveHandler = (event: MouseEvent) => {
+      if (event.target === canvas) {
+        origTouchHandler(event);
+      }
+    };
+    app.touch.attach(canvas);
+
+    const orbitCamera = new OrbitCamera(camera, 1);
+    const orbitCameraInputMouse = new OrbitCameraInputMouse(app, orbitCamera);
+    const orbitCameraInputTouch = new OrbitCameraInputTouch(app, orbitCamera);
+    const orbitCameraInputKeyboard = new OrbitCameraInputKeyboard(
+      app,
+      orbitCamera
+    );
+
+    orbitCamera.focalPoint.snapto(new pc.Vec3(0, 0, 0));
+    orbitCamera.azimElevDistance.snapto(new pc.Vec3(0, -10, 2));
+
     app.root.addChild(camera);
-    camera.setPosition(0, 0, 3);
 
     const loadingMaterial = new pc.StandardMaterial();
     Object.assign(loadingMaterial, physicalDefaultProperties);
@@ -52,6 +108,10 @@ export const usePlayCanvas = (callback: Callback) => {
       sceneData: {
         lights: [],
       },
+      orbitCamera,
+      orbitCameraInputMouse,
+      orbitCameraInputTouch,
+      orbitCameraInputKeyboard,
       canvas,
       app,
       loadingMaterial,
@@ -87,11 +147,13 @@ export const usePlayCanvas = (callback: Callback) => {
   }, [canvas, pcDom]);
 
   const animate = useCallback(
-    (time: number) => {
+    (deltaTime: number) => {
       app.render();
-      savedCallback.current(time);
+      orbitCameraInputKeyboard.update(deltaTime, 1);
+      orbitCamera.update(deltaTime);
+      savedCallback.current(deltaTime);
     },
-    [app]
+    [app, orbitCameraInputKeyboard, orbitCamera]
   );
 
   useEffect(() => {

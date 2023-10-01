ffafda3317383fbfa8fd6a8e23f02d52ae171e9f
Andrew Ray <me@andrewray.me>
2023-10-01 10:52:37 -0700

    hi

--- src/editor/hooks/usePromise.ts
+++ src/editor/hooks/usePromise.ts
@@ -1,61 +1,62 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { useState, useMemo, useEffect, useRef } from 'react'
+import { useState, useMemo, useEffect, useRef } from 'react';
 
-export type PromiseStatus = `pending` | `resolved` | `rejected`
-export type CancelPromise = (message?: string) => void
-export type ResetPromiseState<T> = (keys?: Array<keyof PromiseState<T>>) => void
+export type PromiseStatus = `pending` | `resolved` | `rejected`;
+export type CancelPromise = (message?: string) => void;
+export type ResetPromiseState<T> = (
+  keys?: Array<keyof PromiseState<T>>
+) => void;
 
 export interface PromiseState<T> {
-  status?: PromiseStatus
-  promise?: Promise<T>
-  value?: Awaited<T>
-  error?: Error
-  cancel?: CancelPromise
+  status?: PromiseStatus;
+  promise?: Promise<T>;
+  value?: Awaited<T>;
+  error?: Error;
+  cancel?: CancelPromise;
 }
 
 export type PromiseStateWithReset<T> = PromiseState<T> & {
-  reset: ResetPromiseState<T>
-}
+  reset: ResetPromiseState<T>;
+};
 
 /**
  * A hook which accepts an asynchronous function (i.e., a function which returns a promise).
- * 
+ *
  * Returns a new asynchronous function wrapping the original to be called as necessary,
  * along with the state of the promise as `status`, `promise`, `value`, `error`, `cancel()`,
  * and a `reset()` method to reset the state.
- * 
+ *
  * Pairs well with the {@link hooks.useAsyncExtendedState | `useAsyncExtendedState`} hook.
- * 
+ *
  * Example:
  * ```ts
  * interface State {
  *   foo: string
  *   bar: string
  * }
- * 
+ *
  * const [ state, setState, extendState ] = useAsyncExtendedState<State>({
  *   foo: 'foo',
  *   bar: 'bar'
  * })
- * 
+ *
  * const read = (id: string) => API.client.get(`data/${id}`).then(response => {
  *   return response.data as Partial<State>
  * })
- * 
+ *
  * const [ readRequest, requestRead ] = usePromise(read)
- * 
+ *
  * const isPending = readRequest.status === 'pending'
- * 
+ *
  * return (
  *   <>
  *     <div>
  *       foo: {state.foo}
  *     </div>
- * 
+ *
  *     <div>
  *       bar: {state.bar}
  *     </div>
- * 
+ *
  *     <UI.Button onClick={() => extendState(requestRead('someId'))} disabled={isPending}>
  *       <span>
  *         {isPending
@@ -64,7 +65,7 @@ export type PromiseStateWithReset<T> = PromiseState<T> & {
  *         }
  *       </span>
  *     </UI.Button>
- * 
+ *
  *     <UI.Error>
  *       {readRequest.error}
  *     </UI.Error>
@@ -72,73 +73,108 @@ export type PromiseStateWithReset<T> = PromiseState<T> & {
  * )
  * ```
  */
-export const usePromise = <T extends (...args: any[]) => any>(asyncFunction: T, initialState?: PromiseState<ReturnType<T>>): [ PromiseStateWithReset<ReturnType<T>>, (...asyncFuncArgs: Parameters<T>) => ReturnType<T> ] => {
-  const [ state, setState ] = useState<PromiseState<ReturnType<T>>>(initialState || {})
-  const isCancelled = useRef(false)
-  const isUnmounted = useRef(false)
+export const usePromise = <T extends (...args: any[]) => any>(
+  asyncFunction: T,
+  initialState?: PromiseState<ReturnType<T>>
+): [
+  PromiseStateWithReset<ReturnType<T>>,
+  (...asyncFuncArgs: Parameters<T>) => ReturnType<T>
+] => {
+  const [state, setState] = useState<PromiseState<ReturnType<T>>>(
+    initialState || {}
+  );
+  const isCancelled = useRef(false);
+  const isUnmounted = useRef(false);
 
-  const callAsyncFunction = useMemo(() => (...args: Parameters<T>): ReturnType<T> => {
-    const promise = asyncFunction(...args)
-    const cancel: CancelPromise = message => {
-      isCancelled.current = true
+  const callAsyncFunction = useMemo(
+    () =>
+      (...args: Parameters<T>): ReturnType<T> => {
+        const promise = asyncFunction(...args);
+        const cancel: CancelPromise = (message) => {
+          isCancelled.current = true;
 
-      if (!isUnmounted.current) {
-        setState(({ value }) => ({ status: `rejected`, value, error: message ? new Error(message) : undefined }))
-      }
-    }
+          if (!isUnmounted.current) {
+            setState(({ value }) => ({
+              status: `rejected`,
+              value,
+              error: message ? new Error(message) : undefined,
+            }));
+          }
+        };
 
-    isCancelled.current = false
+        isCancelled.current = false;
 
-    if (promise instanceof Promise) {
-      return new Promise(resolve => {
-        const fulfillPromise = async () => {
-          try {
-            const value: Awaited<ReturnType<T>> = await promise
+        if (promise instanceof Promise) {
+          return new Promise((resolve) => {
+            const fulfillPromise = async () => {
+              try {
+                const value: Awaited<ReturnType<T>> = await promise;
 
-            if (!isCancelled.current && !isUnmounted.current) {
-              setState({ status: `resolved`, value })
-              resolve(value)
-            }
-          } catch (error) {
-            if (!isCancelled.current && !isUnmounted.current) {
-              setState(({ value }) => ({ status: `rejected`, value, error: error instanceof Error ? error : new Error(error as string) }))
-            }
+                if (!isCancelled.current && !isUnmounted.current) {
+                  setState({ status: `resolved`, value });
+                  resolve(value);
+                }
+              } catch (error) {
+                if (!isCancelled.current && !isUnmounted.current) {
+                  setState(({ value }) => ({
+                    status: `rejected`,
+                    value,
+                    error:
+                      error instanceof Error
+                        ? error
+                        : new Error(error as string),
+                  }));
+                }
+              }
+            };
+
+            setState(({ value }) => ({
+              status: `pending`,
+              value,
+              promise,
+              cancel,
+            }));
+            fulfillPromise();
+          }) as ReturnType<T>;
+        } else {
+          setState({ status: `resolved`, value: promise });
+          return promise;
+        }
+      },
+    [asyncFunction]
+  );
+
+  const reset: ResetPromiseState<T> = useMemo(
+    () => (keys) =>
+      setState((state) => {
+        if (!keys) {
+          return {};
+        }
+
+        const nextState = { ...state };
+
+        if (keys) {
+          for (const key of keys) {
+            delete nextState[key];
           }
         }
 
-        setState(({ value }) => ({ status: `pending`, value, promise, cancel }))
-        fulfillPromise()
-      }) as ReturnType<T>
-    } else {
-      setState({ status: `resolved`, value: promise })
-      return promise
-    }
-  }, [ asyncFunction ])
+        return nextState;
+      }),
+    []
+  );
 
-  const reset: ResetPromiseState<T> = useMemo(() => keys => setState(state => {
-    if (!keys) {
-      return {}
-    }
-
-    const nextState = { ...state }
-
-    if (keys) {
-      for (const key of keys) {
-        delete nextState[key]
-      }
-    }
-
-    return nextState
-  }), [])
-
-  const stateWithReset: PromiseStateWithReset<ReturnType<T>> = useMemo(() => ({ ...state, reset }), [ state, reset ])
+  const stateWithReset: PromiseStateWithReset<ReturnType<T>> = useMemo(
+    () => ({ ...state, reset }),
+    [state, reset]
+  );
 
   useEffect(() => {
     return () => {
-      isCancelled.current = true
-      isUnmounted.current = true
-    }
-  }, [])
+      isCancelled.current = true;
+      isUnmounted.current = true;
+    };
+  }, []);
 
-  return [ stateWithReset, callAsyncFunction ]
-}
+  return [stateWithReset, callAsyncFunction];
+};

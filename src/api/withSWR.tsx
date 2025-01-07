b47ee75021db35c8a8ce05ded9913a264fa2bcc8
Andrew Ray <me@andrewray.me>
2025-01-07 09:45:04 -0800

    re-introducing fallback for local dev

--- src/api/withSWR.tsx
+++ src/api/withSWR.tsx
@@ -1,12 +1,15 @@
 import { SWRConfig } from 'swr';
 
-function withSWR<T extends unknown>(WrappedComponent: React.ComponentType<T>) {
+function withSWR<T extends unknown>(
+  WrappedComponent: React.ComponentType<T>,
+  fallback?: any
+) {
   const displayName =
     WrappedComponent.displayName || WrappedComponent.name || 'withSWRWrapper';
 
   const InnerComponent = (props: T & { fallback: any }) => {
     return (
-      <SWRConfig value={{ fallback: props.fallback }}>
+      <SWRConfig value={{ fallback: fallback || props.fallback }}>
         <WrappedComponent {...props} />
       </SWRConfig>
     );
